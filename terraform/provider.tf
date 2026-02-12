d18c2d2f64704753fe8c4d98ed353548e2c520a2
thanhnhu <thanhnhudn@gmail.com>
2026-02-12 17:20:06 +0700

    terraform proxmox provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,14 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.14"
+      source  = "bpg/proxmox"
+      version = "~> 0.50"
     }
   }
 }
+
 
 provider "proxmox" {
   pm_api_url      = var.pm_api_url
