965c0b487c475e0f0798ade92a0645644f5f19fa
thanhnhu <thanhnhudn@gmail.com>
2026-02-12 16:05:42 +0700

    ssh

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,5 +11,7 @@ provider "proxmox" {
   pm_api_url      = var.pm_api_url
   pm_user         = var.pm_user
   pm_password     = var.pm_password
+  #pm_api_token_id = "terraform@pve!terraform-token"
+  #pm_api_token_secret = var.pm_api_token_secret
   pm_tls_insecure = true
 }
